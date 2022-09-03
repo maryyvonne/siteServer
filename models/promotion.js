e96ffcf7d4f4a6ea600a19107f7f26c8764ce773
Mary Varnado <59319147+maryyvonne@users.noreply.github.com>
2022-09-03 09:54:07 -0500

    Create a Schema and Model for Promotions

---
+++ models/promotion.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const promotionSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    featured: {
+      type: Boolean,
+      default: false,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Promotion = mongoose.model("Promotion", promotionSchema);
+export default Promotion;
